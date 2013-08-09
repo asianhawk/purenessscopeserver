9b7fd33db18c250ee09d037a1a5bfb29145ad00e
freeeyes1226@gmail.com <freeeyes1226@gmail.com@f49f508c-0c2f-bbd7-3b2f-f17ad4634cb1>
2013-08-09 06:26:14 +0000

    添加三个Try catch宏用于程序调控。

--- trunk/ purenessscopeserver --username freeeyes1226@gmail.com/example-Module/TcpTest/BaseCommand.cpp
+++ trunk/ purenessscopeserver --username freeeyes1226@gmail.com/example-Module/TcpTest/BaseCommand.cpp
@@ -16,7 +16,16 @@ void CBaseCommand::SetServerObject(CServerObject* pServerObject)
 }
 
 int CBaseCommand::DoMessage(IMessage* pMessage, bool& bDeleteFlag)
-{	
+{
+  //__ENTER_FUNCTION是自动给你的函数追加try的宏，用于捕捉错误，你可以使用__THROW_FUNCTION()抛出你的错误
+  //比如__THROW_FUNCTION("hello"); 它会自动在Log文件夹下的assert.log记录文件名，函数名，行数，描述。
+  //推荐你使用这个宏，记住一定要在函数的结束使用__LEAVE_FUNCTION或者__LEAVE_FUNCTION_WITHRETURN与之配对。
+  //__LEAVE_FUNCTION_WITHRETURN和__LEAVE_FUNCTION不同是前者可以支持函数退出的时候返回一个变量
+  //比如这个函数是返回一个int
+  //那么末尾就是__LEAVE_FUNCTION_WITHRETURN(0); 0就是返回的int，当然，也支持别的类型变量，具体看你自己的函数。
+  //如果函数不返回任何变量，你可以使用__LEAVE_FUNCTION即可。
+  __ENTER_FUNCTION;
+
   if(m_pServerObject == NULL)
   {
     OUR_DEBUG((LM_ERROR, "[CBaseCommand::DoMessage] m_pServerObject is NULL.\n"));
@@ -87,6 +96,7 @@ int CBaseCommand::DoMessage(IMessage* pMessage, bool& bDeleteFlag)
 
   }
 
-
   return 0;
+
+  __LEAVE_FUNCTION_WITHRETURN(0);
 }
