654aa45b1319935306d4e6d409844588ff383068
freeeyes1226@gmail.com <freeeyes1226@gmail.com@f49f508c-0c2f-bbd7-3b2f-f17ad4634cb1>
2013-09-24 01:10:31 +0000

    添加Linux下的后台执行选项，可以通过配置文件配置后台运行。

---  purenessscopeserver --username freeeyes1226@gmail.com/PurenessScopeServer/PurenessScopeServer.cpp
+++  purenessscopeserver --username freeeyes1226@gmail.com/PurenessScopeServer/PurenessScopeServer.cpp
@@ -1,14 +1,90 @@
 // PurenessScopeServer.cpp : 定义控制台应用程序的入口点。
 //
 // 有时候的说，重新站起来绝对不是一件容易的事情，但是我现在必须站起来。要做好样的，freeeyes
-// add by freeeyes, freebird92
+// add by freeeyes
 // 2008-12-22(冬至)
+// 从Twitter的主程序中吸收了一些有用的小技巧，不断融合。
+// 没有目标的坚持是无用的，努力的让PSS更加适合开发，尽量减少开发者开发量，让写代码变的舒适。
+// 并提供更完备的错误分析机制，和一些更好的程序编程技巧。
+// 现在PSS不是一个人在努力了，它有了更多的伙伴，他们会帮助你不断成长。
+// add by freeeyes
+// 2013-09-24
+
 
 #include "MainConfig.h"
 
 #ifndef WIN32
 //如果是Linux
+#include <unistd.h>
 #include "ServerManager.h"
+
+static int daemonize()
+{
+	pid_t pid, sid;
+	int fd;
+
+	pid = fork();
+	switch (pid) {
+	case -1:
+		printf("[daemonize]fork() failed: %s", strerror(errno));
+		return -1;
+
+	case 0:
+		break;
+	default:
+		_exit(0);
+	}
+
+	sid = setsid();
+	if (sid < 0) {
+		printf("[daemonize]setsid() failed: %s", strerror(errno));
+		return -1;
+	}
+
+	if (signal(SIGHUP, SIG_IGN) == SIG_ERR) {
+		printf("[daemonize]signal(SIGHUP, SIG_IGN) failed: %s", strerror(errno));
+		return -1;
+	}
+
+	umask(0);
+
+	fd = open("/dev/null", O_RDWR);
+	if (fd < 0) {
+		printf("[daemonize]open(\"/dev/null\") failed: %s", strerror(errno));
+		return -1;
+	}
+
+	int status = dup2(fd, STDIN_FILENO);
+	if (status < 0) {
+		printf("[daemonize]dup2(%d, STDIN) failed: %s", fd, strerror(errno));
+		close(fd);
+		return -1;
+	}
+
+	status = dup2(fd, STDOUT_FILENO);
+	if (status < 0) {
+		printf("[daemonize]dup2(%d, STDOUT) failed: %s", fd, strerror(errno));
+		close(fd);
+		return -1;
+	}
+
+	status = dup2(fd, STDERR_FILENO);
+	if (status < 0) {
+		printf("[daemonize]dup2(%d, STDERR) failed: %s", fd, strerror(errno));
+		close(fd);
+		return -1;
+	}
+
+	if (fd > STDERR_FILENO) {
+		status = close(fd);
+		if (status < 0) {
+			printf("[daemonize]close(%d) failed: %s", fd, strerror(errno));
+			return -1;
+		}
+	}
+
+	return 0;    
+}
 
 int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
 {
@@ -34,7 +110,9 @@ int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
 	//判断是否是需要以服务的状态启动
 	if(App_MainConfig::instance()->GetServerType() == 1)
 	{
-		ACE::daemonize();
+		//ACE::daemonize();
+		OUR_DEBUG((LM_INFO, "[main]Procress is run background.\n"));
+		daemonize();
 	}
 
 	//第二步，启动主服务器监控
