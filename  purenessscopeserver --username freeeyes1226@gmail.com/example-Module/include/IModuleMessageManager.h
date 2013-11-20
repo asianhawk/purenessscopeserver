b8f8f733c5bf816e240ca9baaf8caf65698fa8f5
freeeyes1226@gmail.com <freeeyes1226@gmail.com@f49f508c-0c2f-bbd7-3b2f-f17ad4634cb1>
2013-11-20 07:33:19 +0000

    添加插件间调用服务器用例。

---  purenessscopeserver --username freeeyes1226@gmail.com/example-Module/include/IModuleMessageManager.h
+++  purenessscopeserver --username freeeyes1226@gmail.com/example-Module/include/IModuleMessageManager.h
@@ -1,13 +1,13 @@
 #ifndef _IMODULEMESSAGEMANAGER_H
 #define _IMODULEMESSAGEMANAGER_H
 
-#include "IMessage.h"
+#include "IBuffPacket.h"
 
 class IModuleMessageManager
 {
 public:
 	virtual ~IModuleMessageManager() {};
-	virtual int SendModuleMessage(const char* pModuleName, uint16 u2CommandID, IBuffPacket* pBuffPacket, bool bDeleteFlag) = 0; //处理模块间的调用关系
+	virtual int SendModuleMessage(const char* pModuleName, uint16 u2CommandID, IBuffPacket* pBuffPacket, IBuffPacket* pReturnBuffPacket) = 0; //处理模块间的调用关系
 };
 
 #endif
