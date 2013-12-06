ad8d2d2a938f8ae2f375950a3bb2ee03940b0bb6
freeeyes1226@gmail.com <freeeyes1226@gmail.com@f49f508c-0c2f-bbd7-3b2f-f17ad4634cb1>
2013-12-06 03:21:33 +0000

    修改了日志根目录读取配置文件错误的BUG

---  purenessscopeserver --username freeeyes1226@gmail.com/PurenessScopeServer/LogSystem/FileLogger.cpp
+++  purenessscopeserver --username freeeyes1226@gmail.com/PurenessScopeServer/LogSystem/FileLogger.cpp
@@ -3,7 +3,7 @@
 CFileLogger::CFileLogger()
 {
 	m_nCount       = 0;
-  m_szLogRoot[0] = '\0';
+	m_szLogRoot[0] = '\0';
 }
 
 CFileLogger::~CFileLogger()
@@ -54,12 +54,12 @@ int CFileLogger::GetLogType(int nIndex)
 bool CFileLogger::Init()
 {
 	CXmlOpeation objXmlOpeation;
-  uint16 u2LogID                  = 0;
+	uint16 u2LogID                  = 0;
 	uint8 u1FileClass               = 0;
 	char szFile[MAX_BUFF_1024]      = {'\0'};
-  char szFileName[MAX_BUFF_100]   = {'\0'};
-  char szServerName[MAX_BUFF_100] = {'\0'};
-  char* pData = NULL;
+	char szFileName[MAX_BUFF_100]   = {'\0'};
+	char szServerName[MAX_BUFF_100] = {'\0'};
+	char* pData = NULL;
 
 	m_vecLogType.clear();
 
@@ -73,59 +73,59 @@ bool CFileLogger::Init()
 	//得到服务器名称
 	//conf.GetValue("ServferName",strServerName,"\\ServerInfo");
 	pData = objXmlOpeation.GetData("ServerLogHead", "Text");
-  if(pData != NULL)
-  {
-    sprintf_safe(szServerName, MAX_BUFF_100, "%s", pData);
-  }
-  OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]strServerName=%s\n", szServerName));	
+	if(pData != NULL)
+	{
+		sprintf_safe(szServerName, MAX_BUFF_100, "%s", pData);
+	}
+	OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]strServerName=%s\n", szServerName));	
 
 	//得到绝对路径
 	pData = objXmlOpeation.GetData("LogPath", "Path");
-  if(pData != NULL)
-  {
-	  sprintf_safe(m_szLogRoot, MAX_BUFF_100, "%s", pData);
-  }
+	if(pData != NULL)
+	{
+		sprintf_safe(m_szLogRoot, MAX_BUFF_100, "%s", pData);
+	}
 	OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]m_strRoot=%s\n", m_szLogRoot));
 
 	//添加子类的个数
-  TiXmlElement* pNextTiXmlElement    = NULL;
-  TiXmlElement* pNextTiXmlElementPos = NULL;
-  TiXmlElement* pNextTiXmlElementIdx = NULL;
+	TiXmlElement* pNextTiXmlElement    = NULL;
+	TiXmlElement* pNextTiXmlElementPos = NULL;
+	TiXmlElement* pNextTiXmlElementIdx = NULL;
 
 	while(true)
 	{
-    pData = objXmlOpeation.GetData("LogInfo", "logid", pNextTiXmlElementIdx);  
-    if(pData != NULL)
-    {
-      u2LogID = (uint16)atoi(pData);                                                      
-      OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]u2LogID=%d\n", u2LogID));
-    }
-    else
-    {
-      break;
-    }
+		pData = objXmlOpeation.GetData("LogInfo", "logid", pNextTiXmlElementIdx);  
+		if(pData != NULL)
+		{
+			u2LogID = (uint16)atoi(pData);                                                      
+			OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]u2LogID=%d\n", u2LogID));
+		}
+		else
+		{
+			break;
+		}
 
 		pData = objXmlOpeation.GetData("LogInfo", "logname", pNextTiXmlElement);
-    if(pData != NULL)
-    {
-      sprintf_safe(szFileName, MAX_BUFF_100, "%s", pData);
-		  OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]strFileValue=%s\n", szFileName));
-    }
-    else
-    {
-      break;
-    }
+		if(pData != NULL)
+		{
+			sprintf_safe(szFileName, MAX_BUFF_100, "%s", pData);
+			OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]strFileValue=%s\n", szFileName));
+		}
+		else
+		{
+			break;
+		}
 
 		pData = objXmlOpeation.GetData("LogInfo", "logtype", pNextTiXmlElementPos);  
-    if(pData != NULL)
-    {
-		  u1FileClass = (uint8)atoi(pData);                                                      
-		  OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]u1FileClass=%d\n", u1FileClass));
-    }
-    else
-    {
-      break;
-    }
+		if(pData != NULL)
+		{
+			u1FileClass = (uint8)atoi(pData);                                                      
+			OUR_DEBUG((LM_ERROR, "[CFileLogger::readConfig]u1FileClass=%d\n", u1FileClass));
+		}
+		else
+		{
+			break;
+		}
 
 		//添加到管理日志文件对象map中
 		mapLogFile::iterator f = m_mapLogFile.find(u2LogID);
@@ -135,7 +135,7 @@ bool CFileLogger::Init()
 			continue;
 		}
 
-		CLogFile* pLogFile = new CLogFile();
+		CLogFile* pLogFile = new CLogFile(m_szLogRoot);
 
 		pLogFile->SetLoggerName(szFileName);
 		pLogFile->SetLoggerType((int)u2LogID);
