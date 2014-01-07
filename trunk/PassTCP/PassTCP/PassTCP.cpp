c97c4d08595f5fe07319bda661d7c06c53797845
freeeyes1226@gmail.com <freeeyes1226@gmail.com@f49f508c-0c2f-bbd7-3b2f-f17ad4634cb1>
2014-01-07 05:17:49 +0000

    添加PassTcp支持文本和二进制块两种发送模式

--- trunk/PassTCP/PassTCP/PassTCP.cpp
+++ trunk/PassTCP/PassTCP/PassTCP.cpp
@@ -36,6 +36,7 @@ CPassTCPApp theApp;
 
 BOOL CPassTCPApp::InitInstance()
 {
+//TODO: call AfxInitRichEdit2() to initialize richedit2 library.
 	// 如果一个运行在 Windows XP 上的应用程序清单指定要
 	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
 	//则需要 InitCommonControlsEx()。否则，将无法创建窗口。
@@ -49,6 +50,10 @@ BOOL CPassTCPApp::InitInstance()
 	CWinAppEx::InitInstance();
 
 	AfxEnableControlContainer();
+
+	AfxInitRichEdit();
+
+	HMODULE h = LoadLibrary(_T("RICHED20.DLL"));
 
 	// 标准初始化
 	// 如果未使用这些功能并希望减小
@@ -72,6 +77,8 @@ BOOL CPassTCPApp::InitInstance()
 		// TODO: 在此放置处理何时用
 		//  “取消”来关闭对话框的代码
 	}
+
+	FreeLibrary(h);
 
 	// 由于对话框已关闭，所以将返回 FALSE 以便退出应用程序，
 	//  而不是启动应用程序的消息泵。
