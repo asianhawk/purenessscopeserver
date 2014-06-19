d6f7233fcf3a7ce30b7bf6cd2e6ebe3e799d8724
freeeyes1226@gmail.com <freeeyes1226@gmail.com@f49f508c-0c2f-bbd7-3b2f-f17ad4634cb1>
2014-06-19 11:56:34 +0000

    修正reactor代码BUG

--- trunk/purenessscopeserver/PurenessScopeServer/Reactor/AceReactorManager.cpp
+++ trunk/purenessscopeserver/PurenessScopeServer/Reactor/AceReactorManager.cpp
@@ -229,28 +229,15 @@ bool CAceReactor::Start()
 {
     OUR_DEBUG((LM_INFO, "[CAceReactor::Start] ReactorID = [%d] ReactorType = [%d] nThreadCount = [%d] Start!\n", GetReactorID(), m_nReactorType, m_nThreadCount));
 
-    if (m_nThreadCount > 0)
+    if (0 == open())
     {
-        if (0 == open())
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return true;
     }
     else
     {
-        if (0 == svc())
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return false;
     }
+
 }
 
 bool CAceReactor::Stop()
@@ -333,10 +320,12 @@ bool CAceReactorManager::AddNewReactor(int nReactorID, int nReactorType, int nTh
         return false;
     }
 
+		/*
     if (nReactorID == REACTOR_CLIENTDEFINE)
     {
         nThreadCount = 0;
     }
+    */
 
     pAceReactor->SetReactorID((uint32)nReactorID);
     bool blState = pAceReactor->Init(nReactorType, nThreadCount, nMaxHandleCount);
@@ -371,12 +360,12 @@ bool CAceReactorManager::StartReactor()
     //先启动非总的Rector
     for (mapAceReactor::iterator b = m_mapAceReactor.begin(); b != m_mapAceReactor.end(); b++)
     {
-        int nReactorID           = (int)b->first;
+        //int nReactorID           = (int)b->first;
         CAceReactor* pAceReactor = (CAceReactor*)b->second;
 
-        if (NULL != pAceReactor && nReactorID != REACTOR_CLIENTDEFINE)
+        if (NULL != pAceReactor)
         {
-            pAceReactor->Start();
+        		pAceReactor->Start();
         }
     }
 
@@ -408,7 +397,9 @@ bool CAceReactorManager::StopReactor()
 {
     for (mapAceReactor::iterator b = m_mapAceReactor.begin(); b != m_mapAceReactor.end(); b++)
     {
+    		int nReactorID           = (int)b->first;
         CAceReactor* pAceReactor = (CAceReactor*)b->second;
+        OUR_DEBUG((LM_ERROR, "[CAceReactorManager::StopReactor]nReactorID=%d.\n", nReactorID));
 
         if (NULL != pAceReactor)
         {
